e8a65db7fc36fe9a7c64024cc04b2658d7cca9bf
Parshav Gandhi <parshav.gandhi@tamasha.live>
2023-02-06 08:52:23 +0530

    added changes

--- pages/contact.js
+++ pages/contact.js
@@ -4,6 +4,12 @@ import Link from "next/link"
 import { siteConfig } from "@/config/site"
 import { Layout } from "@/components/layout"
 import { buttonVariants } from "@/components/ui/button"
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@/components/ui/accordion"
 
 export default function IndexPage() {
   return (
@@ -18,7 +24,29 @@ export default function IndexPage() {
             into your apps. Free. Open Source. And Next.js 13 Ready.
           </p>
         </div>
-      </section>
+
+      <Accordion type="single" collapsible className="w-[450px]">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger>Is it styled?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It comes with default styles that matches the other components'
+          aesthetic.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger>Is it animated?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It's animated by default, but you can disable it if you prefer.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+    </section>
     </Layout>
   )
 }
