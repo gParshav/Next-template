38a1eebf920af40388a581072d622f2dc094a1b7
Parshav Gandhi <parshav.gandhi@tamasha.live>
2023-02-06 08:59:03 +0530

    added changes

--- pages/contact.js
+++ pages/contact.js
@@ -35,14 +35,14 @@ export default function IndexPage() {
       <AccordionItem value="item-2">
         <AccordionTrigger>Is it styled?</AccordionTrigger>
         <AccordionContent>
-          Yes. It comes with default styles that matches the other components'
+          Yes. It comes with default styles that matches the other components
           aesthetic.
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-3">
         <AccordionTrigger>Is it animated?</AccordionTrigger>
         <AccordionContent>
-          Yes. It's animated by default, but you can disable it if you prefer.
+          Yes. Its animated by default, but you can disable it if you prefer.
         </AccordionContent>
       </AccordionItem>
     </Accordion>
